07596f0ffba4550c3a48c853f0b3d3ddb00cecf8
pengjingzhao <1670916827@qq.com>
2025-03-16 16:26:30 +0800

    feat: 增加Jenkinsfile

--- review-service/src/main/java/com/pjz/review/controller/TestDeploy.java
+++ review-service/src/main/java/com/pjz/review/controller/TestDeploy.java
@@ -2,7 +2,9 @@ package com.pjz.review.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/test")
 public class TestDeploy {
 
